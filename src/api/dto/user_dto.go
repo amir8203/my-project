670b69752a386bd0d26cbaaac7999328f3fed848
Mohammad <amirmohammad.tn07@gmail.com>
2024-12-21 19:35:09 +0330

    add user service

--- src/api/dto/user_dto.go
+++ src/api/dto/user_dto.go
@@ -6,3 +6,26 @@ type TokenDetail struct {
 	AccessTokenExpireTime  int    `json:"access_token_expire_time"`
 	RefreshTokenExpireTime int    `json:"refresh_token_expire_time"`
 }
+
+
+type RegisterUserByUsernameRequest struct {
+	Name  string `json:"name" binding:"required,min=6"`
+	Username  string `json:"username" binding:"required,min=5"`
+	Phone     string `json:"phone" binding:"min=6,mobile"`
+	Password  string `json:"password" binding:"required,min=6"`
+}
+
+type UserProfileResponse struct {
+	ID       int64  `json:"id"`           
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Phone    string `json:"phone,omitempty"`
+}
+
+
+type UpdateUserProfileRequest struct {
+	Name     string `json:"name,omitempty" binding:"omitempty,min=6"`
+	Username string `json:"username,omitempty" binding:"omitempty,min=5"`
+	Phone    string `json:"phone,omitempty" binding:"omitempty,min=6,mobile"`
+	Password string `json:"password,omitempty" binding:"omitempty,min=6"`
+}
