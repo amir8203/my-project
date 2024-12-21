f62dd94c8c3c978ff88ddfac6b9f59980125f843
Amir <amirmohammad.tn07@gmail.com>
2024-12-21 20:19:53 +0330

    complete user api

--- src/api/handlers/user.go
+++ src/api/handlers/user.go
@@ -5,17 +5,20 @@ import (
 	"my-project/src/api/dto"
 	"my-project/src/api/helper"
 	"my-project/src/config"
+	"my-project/src/services"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type UserHandler struct {
-	// UserService
+	service *services.UserService
 }
 
 func NewUserHandler(cfg *config.Config) *UserHandler {
+	service := services.NewUserService(cfg)
 	return &UserHandler{
+		service: service,
 	}
 }
 
@@ -29,9 +32,13 @@ func (h *UserHandler) LoginByUsername(c *gin.Context) {
 			helper.GenerateBaseResponseWithValidationError(nil, false, -1, err))
 		return
 	}
-	
-	//call service
-	c.JSON(http.StatusOK, helper.GenerateBaseResponse("response", true, 0))
+	token, err := h.service.LoginByUsername(req.Username, req.Password)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError,
+			helper.GenerateBaseResponseWithError(nil, false, -1, err))
+		return
+	}
+	c.JSON(http.StatusOK, helper.GenerateBaseResponse(token, true, 0))
 }
 
 
@@ -44,8 +51,12 @@ func (h *UserHandler) RegisterByUsername(c *gin.Context) {
 			helper.GenerateBaseResponseWithValidationError(nil, false, -1, err))
 		return
 	}
-	
-	//call service
+	err = h.service.RegisterByUsername(*req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError,
+			helper.GenerateBaseResponseWithError(nil, false, -1, err))
+		return
+	}
 
 	c.JSON(http.StatusCreated, helper.GenerateBaseResponse(nil, true, 0))
 }
@@ -54,17 +65,21 @@ func (h *UserHandler) RegisterByUsername(c *gin.Context) {
 
 
 func (h *UserHandler) ShowProfile(c *gin.Context) {
-	//get user id from jwt then from context
 	userId, exists := c.Get("userId")
 	if !exists {
 		c.AbortWithStatusJSON(http.StatusNotFound,
-			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id= %d not found", userId)))
+			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id not found")))
 		return
 	}
 
-	//call service
+	user, err := h.service.GetInfo(int(userId.(float64)))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound,
+			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id not found")))
+		return
+	}
 
-	c.JSON(http.StatusOK, helper.GenerateBaseResponse("user", true, 0))
+	c.JSON(http.StatusOK, helper.GenerateBaseResponse(user, true, 0))
 }
 
 
@@ -80,12 +95,16 @@ func (h *UserHandler) UpdateProfile(c *gin.Context) {
 	userId, exists := c.Get("userId")
 	if !exists {
 		c.AbortWithStatusJSON(http.StatusNotFound,
-			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id= %d not found", userId)))
+			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id not found")))
 		return
 	}
 
-
-	//call service
+	err = h.service.UpdateUserProfile(*req ,int(userId.(float64)))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound,
+			helper.GenerateBaseResponseWithError(nil, false, -1, err))
+		return
+	}
 
 	c.JSON(http.StatusOK, helper.GenerateBaseResponse("update is successful", true, 0))
 
@@ -97,11 +116,16 @@ func (h *UserHandler) DeleteAccount(c *gin.Context) {
 	userId, exists := c.Get("userId")
 	if !exists {
 		c.AbortWithStatusJSON(http.StatusNotFound,
-			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id= %d not found", userId)))
+			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("user id not found")))
 		return
 	}
 
-	//call service
+	err := h.service.DeleteAccount(int(userId.(float64)))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError,
+			helper.GenerateBaseResponseWithError(nil, false, -1, fmt.Errorf("cant delete account")))
+		return
+	}
 
 	c.JSON(http.StatusOK, helper.GenerateBaseResponse("delete account is successful", true, 0))
 
