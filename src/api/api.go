e316bdcba5b1e258c099bb2db968b7e82f596e80
Amir <amir1103.tr@gmail.com>
2024-12-21 17:37:59 +0330

    add mobile validation

--- src/api/api.go
+++ src/api/api.go
@@ -5,17 +5,33 @@ import (
 	"my-project/src/api/routers"
 	"my-project/src/config"
 
+	validation "my-project/src/api/validations"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 func InitServer(cfg *config.Config) {
 	r := gin.New()
+
+	RegisterValidators()
 
 	r.Use(gin.Logger(), gin.Recovery())
 
 	RegisterRoutes(r, cfg)
 
 	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
+}
+
+
+func RegisterValidators() {
+
+	val, ok := binding.Validator.Engine().(*validator.Validate)
+
+	if ok {
+		val.RegisterValidation("mobile", validation.IranianMobileNumberValidator, true)
+	}
 }
 
 func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
@@ -28,3 +44,4 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		}
 	}
 }
+
